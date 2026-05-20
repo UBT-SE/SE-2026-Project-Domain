a4aa406b7f2dc4ba67307357a8c46163eeb5f10d
Shalini Chakraborty <90648019+shal-C@users.noreply.github.com>
2026-05-20 15:07:18 +0200

    Add root maven project

---
+++ src/test/java/org/campuscoffee/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package org.campuscoffee.runner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "org.campuscoffee.steps"
+)
+public class TestRunner {
+
+}
---
+++ src/test/java/org/campuscoffee/steps/SearchSteps.java
@@ -0,0 +1,11 @@
+package org.campuscoffee.steps;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.Before;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+public class SearchSteps {
+}
